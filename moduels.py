7650e0e97603153d30a915002d8e94b91bfaa8db
rkippin <107101692+rkippin@users.noreply.github.com>
2024-04-13 10:38:07 +0700

    Add files via upload

---
+++ moduels.py
@@ -0,0 +1,6 @@
+import os
+os.system('cd C:\Python27\Scripts & pip install requests')
+os.system('cd C:\Python27\Scripts & pip install bs4')
+os.system('cd C:\Python27\Scripts & pip install telepot')
+os.system('cd C:\Python27\Scripts & pip install colorama')
+os.system('cd C:\Python27\Scripts & pip install pyfiglet')
